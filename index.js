8e051364a11f2192d0b0217e1e23c2d52f7ecd72
ni-shat <nishat.tabassum301@gmail.com>
2023-05-03 16:38:48 +0600

    chefs data api is generated in server

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+var cors = require('cors');
+const chefs = require('./Data/chefs.json');
 
 const app = express();
-
 const port  = 5000;
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send("chef project server is running...");
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 });
 
 app.listen(port , () => {
