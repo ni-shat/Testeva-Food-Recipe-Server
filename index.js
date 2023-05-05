2f978e67a9696074cdd559b128dc2b18bfebad4a
ni-shat <nishat.tabassum301@gmail.com>
2023-05-05 22:55:51 +0600

    latest recipes, and few recipes for chefs is attached

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const chefs = require('./Data/chefs.json');
 const chefDetails = require('./Data/chef-details.json');
+const latestRecipes = require('./Data/latest-recipes.json');
 
 const app = express();
 const port = 5000;
@@ -24,23 +25,11 @@ app.get('/chef-details/:id', (req, res) => {
     res.send(selectedchef);
 });
 
-
-let categories = ["Chicken", "Beef", "Fish", "Dessert", "Pasta"];
-let selectedCategories;
-
-app.get('/recipe-categories/:ct', (req, res) => {
-
-    const ct = req.params.ct;
-    console.log(ct)
-
-    for (const element of chefDetails) {
-        console.log(element?.recipes)
-        selectedRecipes = element?.recipes?.find(c => c.category == 'dessert');
-        console.log(selectedRecipes);
-    }
-
-    res.send(selectedCategories);
+app.get('/latest-recipes', (req, res) => {
+    res.send(latestRecipes);
 });
+
+
 
 app.listen(port, () => {
     console.log(`chef api is running on port: ${port}`);
